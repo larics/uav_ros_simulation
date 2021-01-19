816944939efd487bb7904d03bb07856353ab9085
Lovro Markovic <lovro.markovic@fer.hr>
2021-01-19 15:27:36 +0100

    [ci_test_build.sh] Also build arducopter

--- .ci/ci_test_build.sh
+++ .ci/ci_test_build.sh
@@ -6,8 +6,15 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
 
-echo "Starting test build"
+echo "Starting catkin test build"
 cd ~/uav_ws/src
 source /opt/ros/$ROS_DISTRO/setup.bash
 catkin build
-echo "Ended test build"
+echo "Ended catkin test build"
+
+echo "Starting arducopter build"
+cd ~/uav_ws/src/uav_ros_simulation/.gitman/ardupilot
+export PATH="/usr/lib/ccache:$PATH"
+modules/waf/waf-light configure --board sitl                    
+modules/waf/waf-light build --target bin/arducopter
+echo "Ending arducopter build"
