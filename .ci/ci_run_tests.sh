f42afd65a3e7932965a8d981acc7c97f2c1a2cf2
Lovro Markovic <lovro.markovic@fer.hr>
2021-01-20 11:12:12 +0100

    Update exports for CI

--- .ci/ci_run_tests.sh
+++ .ci/ci_run_tests.sh
@@ -33,6 +33,9 @@ export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$HOME/uav_ws/build/ardupilot_gazeb
 # Manually export ccache to PATH
 export PATH="/usr/lib/ccache:$PATH"
 
+# Add gcc from boards to PATH
+export PATH="/opt/gcc-arm-none-eabi-6-2017-q2-update/bin:$PATH"
+
 echo "Starting running tests"
 cd ~/uav_ws
 catkin build uav_ros_tests
--- .ci/ci_test_build.sh
+++ .ci/ci_test_build.sh
@@ -15,6 +15,7 @@ echo "Ended catkin test build"
 echo "Starting arducopter build"
 cd ~/uav_ws/src/uav_ros_simulation/.gitman/ardupilot
 export PATH="/usr/lib/ccache:$PATH"
+export PATH="/opt/gcc-arm-none-eabi-6-2017-q2-update/bin:$PATH"
 modules/waf/waf-light configure --board sitl                    
 modules/waf/waf-light build --target bin/arducopter
 echo "Ending arducopter build"
