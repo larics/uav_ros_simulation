78b463fb6ca9f29c8b2532e276c18e8f5d4abb11
Lovro Markovic <lovro.markovic@fer.hr>
2021-02-15 17:59:27 +0100

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -9,24 +9,6 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # Executes a command when ERR signal is emmitted in this script
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-sudo apt-get update -qq
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends tzdata
-sudo apt-get install -y --no-install-recommends dialog apt-utils
-sudo apt-get -y install gnupg2 libterm-readline-gnu-perl lsb-release
-
-sudo apt-get update -qq
-
-# the "gce-compute-image-packages" package often freezes the installation at some point
-# the installation freezes when it tries to manage some systemd services
-# this attempts to install the package and stop the problematic service during the process
-((sleep 90 && (sudo systemctl stop google-instance-setup.service && echo "gce service stoped" || echo "gce service not stoped")) & (sudo timeout 120s apt-get -y install gce-compute-image-packages)) || echo "\e[1;31mInstallation of gce-compute-image-packages failed\e[0m"
-
-distro=`lsb_release -r | awk '{ print $2 }'`
-[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
-[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
-[ "$distro" = "20.04" ] && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends keyboard-configuration
-
-sudo apt-get -y upgrade --fix-missing
 sudo apt-get -y install git
 
 # get the path to this script
