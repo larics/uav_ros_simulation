4fbfeef6c4c188dd60333d3a881a235d10764190
Lovro Markovic <lovro.markovic@fer.hr>
2021-05-04 22:50:42 +0200

    Fix #5

--- installation/unpack_modules.sh
+++ installation/unpack_modules.sh
@@ -31,6 +31,19 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
+if [ "$#" -ne 1 ]; then
+    echo "One argument expected - path to the 'src' folder of the Catkin Workspace"
+    exit 2
+fi
+
+CATKIN_SRC=$1
+
+# Check if this is a directory
+if [[ ! -d "$CATKIN_SRC" ]]; then
+    echo "$CATKIN_SRC is not a directory"
+    exit 2
+fi
+
 cd $MY_PATH/..
 
 echo "Uninstalling gitman modules in $(pwd)"
@@ -54,5 +67,13 @@ echo "Updating gitman.yml with new location"
 sed -i '1s@.*@location: ../../uav_ros_stack_modules@' gitman.yml
 gitman update -f
 touch "CATKIN_IGNORE"
+
+SIM_PATH=$(readlink -f "$MY_PATH/../../uav_ros_simulation")
+SIM_MODULES_PATH=$(readlink -f "$MY_PATH/../../uav_ros_simulation_modules")
+STACK_MODULES_PATH=$(readlink -f "$MY_PATH/../../uav_ros_stack_modules")
+
+ln -s $SIM_PATH $CATKIN_SRC/uav_ros_simulation
+ln -s $SIM_MODULES_PATH $CATKIN_SRC/uav_ros_simulation_modules
+ln -s $STACK_MODULES_PATH $CATKIN_SRC/uav_ros_stack_modules
 
 cd $MY_PATH
