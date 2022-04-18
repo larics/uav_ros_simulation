3817767c3bf37d8dd7c580e644ae198fe772d3bc
Lovro Markovic <lovro.markovic@fer.hr>
2022-04-18 17:40:04 +0200

    Add kopterworx unity startup folder

---
+++ startup/kopterworx_unity/unity_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Unity settings
+export PATH="${HOME}/Unity/Hub/Editor/2020.3.33f1/Editor":$PATH
+export UNITY_SETTINGS=custom_config/settings.json
+export AIRSIM_PROJECT=$HOME/Github/AirSim/Unity/UnityDemo
+
+# Stack settins
+export UAV_NAMESPACE=red
