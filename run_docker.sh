5d22a5434226f5da9a89acfc6043a15894451ca4
JakobDomislovic <jakob.domislovic2@gmail.com>
2024-01-14 09:13:43 +0100

    [run_docker] Add ssh forwarding, add --enable-gpu flag

--- run_docker.sh
+++ run_docker.sh
@@ -11,10 +11,15 @@ CONTAINER_NAME=uav_ros_simulation
 # Get distro of the built image
 distro="focal-bin-0.0.1"
 run_args=""
+gpu_enabled=""
 
 for (( i=1; i<=$#; i++));
 do
   param="${!i}"
+
+  if [ "$param" == "--enable-gpu" ]; then
+    gpu_enabled="--gpus all"
+  fi
 
   if [ "$param" == "--bionic" ]; then
     distro="bionic"
@@ -35,11 +40,13 @@ do
 
 done
 
+run_args="$gpu_enabled $run_args"
+
 echo "Running in $distro"
 
 # Check if there is an already running container with the same distro
-full_container_name="${CONTAINER_NAME}_${distro}"
-running_container="$(docker container ls -al | grep $full_container_name)"
+full_container_name="${CONTAINER_NAME}_${distro}_new"
+running_container="$(docker container ls --all | grep $full_container_name)"
 if [ -z "$running_container" ]; then
   echo "Running $full_container_name for the first time!"
 else
@@ -49,23 +56,23 @@ else
   exit 0
 fi
 
-# Check if using GPU
-gpu_enabled="--gpus all"
-if [ "$distro" == "focal-nogpu" ]; then
-  gpu_enabled=""
-fi
-run_args="$gpu_enabled $run_args"
+# Hook to the current SSH_AUTH_LOCK - since it changes
+# https://www.talkingquickly.co.uk/2021/01/tmux-ssh-agent-forwarding-vs-code/
+ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
 
+set -x
 docker run \
   $run_args \
   -it \
   --network host \
   --privileged \
+  --volume ~/.ssh/ssh_auth_sock:/ssh-agent \
+  --env SSH_AUTH_SOCK=/ssh-agent \
   --volume=$XSOCK:$XSOCK:rw \
   --volume=$XAUTH:$XAUTH:rw \
   --env="XAUTHORITY=${XAUTH}" \
   --env DISPLAY=$DISPLAY \
   --env TERM=xterm-256color \
   --name $full_container_name \
-  uav_ros_simulation:$distro \
+  lmark1/uav_ros_simulation:$distro \
   /bin/bash
